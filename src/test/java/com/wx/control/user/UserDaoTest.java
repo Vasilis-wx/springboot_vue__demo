2e5c9f50f4b3f9913f56d28b23d42107c7a82477
Vasilis-wx <1477394687@qq.com>
2018-10-11 19:55:48 +0800

    插入100000条数据

--- src/test/java/com/wx/control/user/UserDaoTest.java
+++ src/test/java/com/wx/control/user/UserDaoTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -35,5 +36,22 @@ public class UserDaoTest {
     public void finall(){
 //        List<FUser> LIST = userDao.findAllUser2();
 //        System.out.printf(LIST.size()+"");
+    }
+
+
+    @Test
+    public void addUserTest(){
+        for(int i = 5800 ; i < 100000 ;i++ ){
+            TUser user = new TUser();
+            user.setId(i);
+            user.setName("测试");
+            user.setPhone("1212121212212");
+            user.setSex(1);
+            user.setUserface("4545454545");
+            user.setPassword("666");
+            user.setBirthday(new Date());
+
+            userDao.save(user);
+        }
     }
 }
